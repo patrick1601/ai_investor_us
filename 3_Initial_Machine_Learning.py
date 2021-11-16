021978b4fdcee46fbada00983a2a44387f642d5d
patrick.petanca <ppetanca@gmail.com>
2021-11-15 17:29:01 -0700

    trained Knn model

--- 3_Initial_Machine_Learning.py
+++ 3_Initial_Machine_Learning.py
@@ -58,8 +58,8 @@ print('Train MSE: ',
 print('Test MSE: ',
       mean_squared_error(y_test, y_pred))
 
-#import pickle # To save the fitted model
-#pickle.dump(pl_linear, open("pl_linear.p", "wb" ))
+import pickle # To save the fitted model
+pickle.dump(pl_linear, open("pl_linear.p", "wb" ))
 #%% try many runs to see mean squared error with different training set sizes
 from sklearn.model_selection import learning_curve
 from sklearn.model_selection import ShuffleSplit
@@ -156,7 +156,7 @@ print('Actual Top 10 Returns:', round(y_results['Actual Return'].iloc[:10].mean(
 # See bottom 10 stocks and see how the values differ
 print('\nBottom 10 Predicted Returns:', round(y_results['Predicted Return'].iloc[-10:].mean(),2) , '%','\n')
 print('Actual Bottom 10 Returns:', round(y_results['Actual Return'].iloc[-10:].mean(),2) , '%','\n')
-#%% function to try a few top10/bottom10 runs
+#%% function to try a few top10/bottom10 runs by changing the random state of the test/train split
 def observePredictionAbility(my_pipeline, X, y, returnSomething=False, verbose=True):
     '''
     For a given predictor pipeline.
@@ -329,3 +329,158 @@ def PrintTopAndBottom10Predictions(y_test, y_pred):
 PrintTopAndBottom10Predictions(y_test, y_pred)
 #%% see if the results are repeatabile
 observePredictionAbility(pl_ElasticNet, X, y)
+#%% K NEAREST NEIGHBOURS REGRESSION
+# Read in data and do train/test split.
+X, y = loadXandyAgain()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+print('X training matrix dimensions: ', X_train.shape)
+print('X testing matrix dimensions: ', X_test.shape)
+print('y training matrix dimensions: ', y_train.shape)
+print('y testing matrix dimensions: ', y_test.shape)
+#%%
+# KNeighbors regressor
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import PowerTransformer
+pl_KNeighbors = Pipeline([
+    ('Power Transformer', PowerTransformer()),
+    ('KNeighborsRegressor', KNeighborsRegressor(n_neighbors=40))
+])
+
+pl_KNeighbors.fit(X_train, y_train)
+y_pred = pl_KNeighbors.predict(X_test)
+from sklearn.metrics import mean_squared_error
+print('train mse: ', mean_squared_error(y_train, pl_KNeighbors.predict(X_train)))
+print('test mse: ', mean_squared_error(y_test, y_pred))
+
+import pickle
+pickle.dump(pl_KNeighbors, open("pl_KNeighbors.p", "wb" ))
+#%% check validation curve
+knn_validation_list = []
+numNeighbours = [4, 8, 16, 32, 64, 100]
+runNum = 40
+
+for i in numNeighbours:
+    print('Trying K=' + str(i))
+    for j in range(0, runNum):
+        # Get a new train/test split
+        X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                            test_size=0.1,
+                                                            random_state=42 + j)
+        pl_KNeighbors = Pipeline([
+            ('Power Transformer', PowerTransformer()),
+            ('KNeighborsRegressor', KNeighborsRegressor(n_neighbors=i))
+        ]).fit(X_train, y_train)
+
+        y_pred = pl_KNeighbors.predict(X_test)
+
+        resultThisRun = [i,
+                         mean_squared_error(y_train, pl_KNeighbors.predict(X_train)),
+                         mean_squared_error(y_test, y_pred)]
+
+        knn_validation_list.append(resultThisRun)
+
+knn_validation_df = pd.DataFrame(knn_validation_list,
+                                 columns=['numNeighbours',
+                                          'trainError',
+                                          'testError'])
+#%% create dataframe to plot results
+# Get our results in a format we can easily plot
+knn_results_list = []
+numNeighboursAttempted = knn_validation_df['numNeighbours'].unique()
+results_df = pd.DataFrame(index=numNeighboursAttempted)  # Create a DataFrame of results
+
+for i in numNeighboursAttempted:
+    blNeighbours = knn_validation_df['numNeighbours'] == i  # boolean mask
+
+    trainErrorsMean = knn_validation_df[blNeighbours]['trainError'].mean()
+    trainErrorsStd = knn_validation_df[blNeighbours]['trainError'].std()
+    testErrorsMean = knn_validation_df[blNeighbours]['testError'].mean()
+    testErrorsStd = knn_validation_df[blNeighbours]['testError'].std()
+    knn_results_list.append([trainErrorsMean, trainErrorsStd,
+                             testErrorsMean, testErrorsStd])
+
+knn_results_df = pd.DataFrame(knn_results_list,
+                              columns=['trainErrorsMean', 'trainErrorsStd',
+                                       'testErrorsMean', 'testErrorsStd'],
+                              index=numNeighboursAttempted)
+#%%
+knn_results_df['trainErrorsMean'].plot(style='-x', figsize=(7,4.5))
+knn_results_df['testErrorsMean'].plot(style='-x')
+
+plt.fill_between(knn_results_df.index,\
+                 knn_results_df['trainErrorsMean']
+                 -knn_results_df['trainErrorsStd'],
+                 knn_results_df['trainErrorsMean']
+                 +knn_results_df['trainErrorsStd'], alpha=0.2)
+plt.fill_between(results_df.index,\
+                 knn_results_df['testErrorsMean']
+                 -knn_results_df['testErrorsStd'],
+                 knn_results_df['testErrorsMean']
+                 +knn_results_df['testErrorsStd'], alpha=0.2)
+plt.grid()
+plt.legend(['Training CV MSE Score','Test Set MSE'])
+plt.ylabel('MSE', fontsize=15)
+plt.xlabel('Values for K', fontsize=15)
+plt.title('K-NN Validation Curve', fontsize=20)
+#plt.ylim([0, 0.8]);
+plt.show()
+#%% plot the scatter graph
+plt.figure(figsize=(6,6))
+plotDensityContourPredVsReal('pl_KNeighbors', y_pred, y_test.to_numpy(),2)
+#%% See top 10 stocks and see how the values differ
+PrintTopAndBottom10Predictions(y_test, y_pred)
+#%%
+observePredictionAbility(pl_KNeighbors, X, y)
+#%% KDE Plot
+# Output scatter plot and contour plot of density of points to see if prediciton matches reality
+# Line of x=y is provided, perfect prediction would have all density on this line
+# Also plot linear regression of the scatter
+
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import kde # Need for Kernel Density Calculation
+from sklearn.linear_model import LinearRegression
+
+def plotDensityContourPredVsRealContour(model_name, x_plot, y_plot, ps):
+#x_plot, y_plot = y_pred, y_test.to_numpy()
+    resolution = 40
+    # Make a gaussian kde on a grid
+    k = kde.gaussian_kde(np.stack([x_plot,y_plot]))
+    xi, yi = np.mgrid[-ps:ps:resolution*4j, -ps:ps:resolution*4j]
+    zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+    # Plotting scatter and contour plot
+    plt.pcolormesh(xi,
+                   yi,
+                   zi.reshape(xi.shape),
+                   shading='gouraud',
+                   cmap=plt.cm.Greens)
+    plt.contour(xi, yi, zi.reshape(xi.shape) )
+    plt.scatter(x_plot, y_plot, s=1)
+    # Plotting linear regression
+    LinMod = LinearRegression()
+    LinMod.fit(y_plot.reshape(-1, 1), x_plot.reshape(-1, 1),)
+    xx=[[-2],[2]]
+    yy=LinMod.predict(xx)
+    plt.plot(yy,xx)
+    # Plot formatting
+    plt.grid()
+    plt.axhline(y=0, color='r', label='_nolegend_')
+    plt.axvline(x=0, color='r', label='_nolegend_')
+    plt.xlabel('Predicted Return')
+    plt.ylabel('Actual Return')
+    plt.plot([-100,100],[-100,100],'y--')
+    plt.xlim([-ps*0.2,ps*0.6]) #
+    plt.ylim([-ps,ps])
+    plt.title('Predicted/Actual density plot for {}'.format(model_name))
+    plt.legend([
+        'Linear Fit Line','y=x Perfect Prediction Line',
+        'Prediction Points'])
+    # Save Figure
+    plt.savefig('result.png')
+#%%
+plt.figure(figsize=(7,7))
+plotDensityContourPredVsRealContour('pl_KNeighbors', y_pred, y_test.to_numpy(),1)
